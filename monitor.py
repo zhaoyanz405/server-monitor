f4f8930f54693193a60743677cadf78a93a37a46
zhaoy <yan.zhao@idiaoyan.com>
2019-10-12 11:37:15 +0800

    feat： 修改了发送邮件逻辑

--- monitor.py
+++ monitor.py
@@ -15,7 +15,7 @@ from datetime import datetime
 import psutil
 import yaml
 
-from ems import send_instant_mail
+from ems import send_instant_mail, STATUS_MAIL_SEND_SUCCEED
 
 file_path = os.path.dirname(__file__)
 
@@ -168,10 +168,35 @@ def send(config, lines):
     :return:
     """
     mail_server = get_mail_server(config)
-    status = send_instant_mail(mail_server=mail_server, mail_from='', subject=config.get('subject'),
+    mail = config.get('mail')
+    account = config.get('account')
+    status = send_instant_mail(mail_server=mail_server, mail_from=account.get('user'), subject=mail.get('subject'),
                                content='\n'.join(lines),
-                               mail_to=config.get('mail_to'), copy_to=config.get('cc'))
-    print(status)
+                               mail_to=mail.get('to'), copy_to=mail.get('cc'))
+    if status == STATUS_MAIL_SEND_SUCCEED:
+        print('[send email] success')
+    else:
+        print('[send email] fail.')
+        print('status:', status)
+        print("""
+            STATUS_MAIL_SEND_FAILED = -1  # 失败的
+            STATUS_MAIL_SEND_SUCCEED = 1  # 成功的
+            STATUS_MAIL_SERVER_ERROR = 2  # 邮件服务器配置错误
+            STATUS_MAIL_SERVER_HOST_ERROR = 3  # 邮件服务器host为空
+            STATUS_MAIL_SERVER_USERNAME_ERROR = 4  # 邮件服务器用户名为空
+            STATUS_MAIL_SERVER_PASSWORD_ERROR = 5  # 邮件服务器密码为空
+            STATUS_MAIL_SUBJECT_NONE_ERROR = 6  # 邮件主题为空
+            STATUS_MAIL_SUBJECT_PARAMS_ERROR = 7  # 邮件主题参数格式错误
+            STATUS_MAIL_FROM_ADDRESS_NONE_ERROR = 8  # 邮件发送地址为空
+            STATUS_MAIL_FROM_ADDRESS_PARAMS_ERROR = 9  # 邮件发送地址参数格式错误
+            STATUS_MAIL_TO_ADDRESS_NONE_ERROR = 10  # 邮件目标地址为空
+            STATUS_MAIL_TO_ADDRESS_PARAMS_ERROR = 11  # 邮件目标地址参数格式错误
+            STATUS_MAIL_CONTENT_NONE_ERROR = 12  # 邮件内容为空
+            STATUS_MAIL_CONTENT_PARAMS_ERROR = 13  # 邮件内容参数错误
+            STATUS_MAIL_HELO_ERROR = 14  # 无法连接到邮件服务器
+            STATUS_MAIL_REJECTED_ALL_ERROR = 15  # 服务器拒绝所有接收方
+            STATUS_MAIL_SENDER_REFUSED_ERROR = 16  # 服务器拒绝发送地址请求
+            STATUS_MAIL_UNEXPECTED_CODE_ERROR = 17  # 服务器返回其他错代码""")
 
 
 def load_monitor():
@@ -205,7 +230,7 @@ def usage():
         python monitor.py [options]
         
         -h  --help 
-        -t  test the config
+        -t  --test test the config, and send confirm email.
         --start  run monitor
         --stop   stop monitor
         
@@ -221,6 +246,9 @@ if __name__ == '__main__':
     except getopt.GetoptError:
         usage()
         sys.exit(-1)
+
+    if not options:
+        usage()
 
     for name, value in options:
         if name in ("-h", "--help"):
